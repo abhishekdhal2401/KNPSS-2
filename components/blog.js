64fd9b2167e07cf9b55c2157eb61adcbe317254a
harisumaid27 <harisumaid27@gmail.com>
2020-11-16 14:24:02 +0530

    bugs fixes on blog post component

--- components/blog.js
+++ components/blog.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/Blog.module.css";
-import { Icon, Image, Divider } from "semantic-ui-react";
+import { Icon, Image, Divider, Header } from "semantic-ui-react";
 import { Carousel } from "react-responsive-carousel";
 import Navbar from "../components/navbar";
 const Blog = ({ post }) => {
@@ -13,31 +13,37 @@ const Blog = ({ post }) => {
             <Icon name="blind" size="huge" />
             <span>KNPSS</span>
           </div>
-  <Divider horizontal >{post.heading}</Divider>
+          <Header dividing>{post.heading}</Header>
           <div className={styles.heading}>
             {/* <h1>{post.heading}</h1> */}
             <p>{post.date.split("").reverse().join("")}</p>
           </div>
           <Divider />
           <div className={styles.images}>
-            <Carousel
-              autoPlay={true}
-              infiniteLoop={true}
-              showThumbs={false}
-              dynamicHeight
-              showIndicators={false}
-              swipeable
-              useKeyboardArrows
-            >
-              {post.imagesPath.map((path) => {
-                return (
-                  <div key={path}>
-                    <Image src={path} />
-                    <p className="post-read"></p>
-                  </div>
-                );
-              })}
-            </Carousel>
+          <Carousel
+            autoPlay={true}
+            infiniteLoop={true}
+            showThumbs={true}
+            dynamicHeight
+            showIndicators={true}
+            swipeable
+            useKeyboardArrows
+            // renderThumbs={() =>
+            //   imageList.map((image) => (
+            //     <img key={`thumbs ${image}`} src={image}></img>
+            //   ))
+            // }
+          >
+            {post.imagesPath.map((path) => {
+              return (
+                <div key={path}>
+                  <a href={path} target="_blank" type="image/*">
+                    <Image src={decodeURI(path)} width={600} height={400} />
+                  </a>
+                </div>
+              );
+            })}
+          </Carousel>
           </div>
           <Divider />
           <div className={styles.document}>
@@ -50,7 +56,9 @@ const Blog = ({ post }) => {
                   return (
                     <li className={styles.pdf} key={path}>
                       {" "}
-                      <a target='_blank' href={path}>{path.slice(path.search('_')+1)}</a>{" "}
+                      <a target="_blank" href={path}>
+                        {decodeURI(path.slice(path.search("_") + 1))}
+                      </a>{" "}
                     </li>
                   );
                 })
